ed602026680ab823e857ef77bfb6e754d681ae80
AliCharper <alikolahdoozan@gmail.com>
2022-06-13 07:21:57 +0800

    Part 6

--- Bad_TaxCalculator.Test/IndividualTaxCalculatorTest.cs
+++ Bad_TaxCalculator.Test/IndividualTaxCalculatorTest.cs
@@ -5,44 +5,70 @@ namespace Bad_TaxCalculator.Test
     public class IndividualTaxCalculatorTest
     {
 
+
+        private TaXCalculator _calculator = new TaXCalculator();
+
         [Fact]
-        public void Disable_Taxpayer_SHOULD_Pay_Zero_Percent_Tax()
+        public void Retired_Taxpayer_SHOULD_Pay_ONE_Percent_Tax()
         {
             //ARRANGE
             TaxPayer taxPayer = new TaxPayer
             {
-                TaxCitizen=true,
-                HasDisability = true
+                TaxCitizen = true,
+                HasDisability = false,
+                IsRetired = true
             };
-            IndividualTaxCalculator individualTaxCalculator = new IndividualTaxCalculator();
 
             //Act
-           var Result= individualTaxCalculator.CalculateTaxPercentage(taxPayer);
+            var result = _calculator.CalculateTaxPercentage(taxPayer);
 
             //Assert
-            Assert.Equal(0, Result);
+            Assert.Equal(1, result);
         }
 
 
-        [Fact]
-        public void None_TaxResident_SHOULD_NOT_BE_Calculated()
-        {
-            //ARRANGE
-            TaxPayer taxPayer = new TaxPayer
-            {
-                TaxCitizen = false
-            };
-            IndividualTaxCalculator individualTaxCalculator = new IndividualTaxCalculator();
 
-            //Act
-            string expectedErrorMessage = "No TAX Calculation for NON-TAX Residents";
 
-            var Exception_Result = Assert.Throws<InvalidOperationException>(() =>
-            individualTaxCalculator.CalculateTaxPercentage(taxPayer));
 
-            //Assert
-            Assert.Equal(expectedErrorMessage, Exception_Result.Message);
-        }
+
+        //[Fact]
+        //public void Disable_Taxpayer_SHOULD_Pay_Zero_Percent_Tax()
+        //{
+        //    //ARRANGE
+        //    TaxPayer taxPayer = new TaxPayer
+        //    {
+        //        TaxCitizen=true,
+        //        HasDisability = true
+        //    };
+        //    IndividualTaxCalculator individualTaxCalculator = new IndividualTaxCalculator();
+
+        //    //Act
+        //   var Result= individualTaxCalculator.CalculateTaxPercentage(taxPayer);
+
+        //    //Assert
+        //    Assert.Equal(0, Result);
+        //}
+
+
+        //[Fact]
+        //public void None_TaxResident_SHOULD_NOT_BE_Calculated()
+        //{
+        //    //ARRANGE
+        //    TaxPayer taxPayer = new TaxPayer
+        //    {
+        //        TaxCitizen = false
+        //    };
+        //    IndividualTaxCalculator individualTaxCalculator = new IndividualTaxCalculator();
+
+        //    //Act
+        //    string expectedErrorMessage = "No TAX Calculation for NON-TAX Residents";
+
+        //    var Exception_Result = Assert.Throws<InvalidOperationException>(() =>
+        //    individualTaxCalculator.CalculateTaxPercentage(taxPayer));
+
+        //    //Assert
+        //    Assert.Equal(expectedErrorMessage, Exception_Result.Message);
+        //}
 
 
     }
--- Bad_TaxCalculator/IndividualTaxCalculator.cs
+++ Bad_TaxCalculator/IndividualTaxCalculator.cs
@@ -7,6 +7,60 @@ using System.Threading.Tasks;
 
 namespace Bad_TaxCalculator
 {
+    public interface ITaxRule
+    {
+        int CalculateTaxPercentage(TaxPayer taxPayer, int currentPercentage);
+    }
+    //
+    public class RetiredRule : ITaxRule
+    {
+        public int CalculateTaxPercentage(TaxPayer taxPayer, int currentPercentage)
+        {
+            if (taxPayer.IsRetired)
+
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+    //
+    public class TaxRuleEngine
+    {
+        List<ITaxRule> _rules = new List<ITaxRule>();
+
+        public TaxRuleEngine(IEnumerable<ITaxRule> rules)
+        {
+            _rules.AddRange(rules);
+        }
+
+        public int CalculateTaxPercentage(TaxPayer taxPayer)
+        {
+            int TaxPercentage = 0;
+            foreach (var rule in _rules)
+            {
+                TaxPercentage = Math.Max(TaxPercentage, rule.CalculateTaxPercentage(taxPayer, TaxPercentage));
+            }
+            return TaxPercentage;
+        }
+    }
+    //
+    public class TaXCalculator
+    {
+        public int CalculateTaxPercentage(TaxPayer taxPayer)
+        {
+            var ruleType = typeof(ITaxRule);
+            IEnumerable<ITaxRule> rules = this.GetType().Assembly.GetTypes()
+                .Where(p => ruleType.IsAssignableFrom(p) && !p.IsInterface)
+                .Select(r => Activator.CreateInstance(r) as ITaxRule);
+
+            var engine = new TaxRuleEngine(rules);
+
+            return engine.CalculateTaxPercentage(taxPayer);
+        }
+    }
+
+    //
     public class IndividualTaxCalculator
     {
         public int CalculateTaxPercentage(TaxPayer taxPayer)
